1694c977683bfae39eacecd27d57281a6c71c4cf
Jose Damian jldamians <jlds161089@gmail.com>
2020-03-30 18:24:54 -0500

    [update] Implementando ejemplos

---
+++ examples/ClosePrimaryTaxpayerDialogs.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const puppeteer = require('puppeteer');
+const LoginSOL = require('sol-login');
+
+const PopupSOL = require('../lib')
+
+class SUNAT extends PopupSOL {
+  constructor(page=null) {
+    super(page);
+  }
+
+  async login() {
+    const sol = await new LoginSOL(this.page);
+
+    await sol.signin(...arguments);
+  }
+}
+
+const chromeOptions = {
+  headless: false,
+  slowMo: 10,
+  defaultViewport: null
+};
+
+(async function main() {
+  const browser = await puppeteer.launch(chromeOptions);
+
+  const page = await browser.newPage();
+
+  await page.goto('https://e-menu.sunat.gob.pe/cl-ti-itmenu/MenuInternet.htm');
+
+  const sunat = new SUNAT(page);
+
+  await sunat.login('10066920939', 'DEMANCLA', 'gelissidd');
+
+  await sunat.close();
+})();
+
